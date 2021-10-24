a686fd7d3b1a1c6c32655c1a230536726cee327f
orange2008 <40315138+orange2008@users.noreply.github.com>
2021-10-24 10:58:33 +0800

    fix connection

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// MySQL credentials
-$db_host = 'oracle-arm-jp.servers.frank-ruan.com';
-$db_user = 'filedb';
-$db_pass = 'rWJMcrSJJ4PAcLMK';
-$db_name = 'filedb';
-?>
--- handler.php
+++ handler.php
@@ -1,7 +1,10 @@
 <?php
-// Read config.php
-require('config.php');
-// Check the connection to MySQL, credentials are in config.php
+// MySQL credentials
+$db_host = 'oracle-arm-jp.servers.frank-ruan.com';
+$db_user = 'filedb';
+$db_pass = 'rWJMcrSJJ4PAcLMK';
+$db_name = 'filedb';
+// Check the connection to MySQL
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
